eaac435585054db582c9ece63c47c26424b7cb4a
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2025-01-29 09:38:20 -0600

    Update 1/29

--- player_3d.gd
+++ player_3d.gd
@@ -1,10 +1,17 @@
 extends CharacterBody3D
 
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.5
 const CAM_SENSITIVITY = 0.03
-const SPEED = 5.0
+var SPEED = WALK_SPEED
 
 const JUMP_VELOCITY = 7.5
 @onready var camera = $Head/Camera3D
+@onready var camera_arm = $SpringArm3D
+@onready var camera_pos = camera.position
+
+@onready var BASE_FOV = camera.fov
+var FOV_CHANGE = 1.0
 
 var first_person = true
 
@@ -22,7 +29,10 @@ func _unhandled_input(event: InputEvent) -> void:
 			self.rotate_y(-event.relative.x * ( CAM_SENSITIVITY / 10.0))
 			camera.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
 			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-40), deg_to_rad(60))
-			
+		else: 
+			self.rotate_y(-event.relative.x * ( CAM_SENSITIVITY / 10.0))
+			camera_arm.rotate_x(-event.relative.y * (CAM_SENSITIVITY / 10.0))
+			camera_arm.rotation.x = clamp(camera_arm.rotation.x, deg_to_rad(-75), deg_to_rad(75))
 pass
 
 func _physics_process(delta: float) -> void:
@@ -31,29 +41,57 @@ func _physics_process(delta: float) -> void:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else: 
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
+	if Input.is_action_just_pressed("change_camera"): # V
+		toggle_camera_parent()
+
+		if Input.is_action_just_pressed("sprint"):
+			SPEED = SPRINT_SPEED
+			FOV_CHANGE = 2.0
+		else:
+			SPEED = WALK_SPEED
+			FOV_CHANGE = 1.0
+
+	# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY 
+		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+			velocity.y = JUMP_VELOCITY 
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+		var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+		if direction:
+			velocity.x = direction.x * SPEED
+			velocity.z = direction.z * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	t_bob += delta * velocity.length() * float(is_on_floor())
-	camera.transform.origin = headbob(t_bob)
+		var velocity_clamped = clamp(velocity.length(), 0.5, SPEED * 2)
+		var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+		camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
 
-	move_and_slide()
+		t_bob += delta * velocity.length() * float(is_on_floor())
+		camera.transform.origin = headbob(t_bob)
+
+		move_and_slide()
+
+func toggle_camera_parent():
+	var parent = "Head"
+	if first_person:
+		parent = "SpringArm3D"
+		# TODO: model visible
+	var child = camera
+	child.get_parent().remove_child(child)
+	get_node(parent).add_child(child)
+	camera = child
+	if not first_person:
+		camera.position = camera_pos
+		# TODO: model invisible
+	first_person = not first_person # false -> true or true -> false
 
 func headbob(time):
 	var pos = Vector3.ZERO
