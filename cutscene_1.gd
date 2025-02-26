58ec9ca96bfbb8c378ec8f2da4b5146c63debf32
janssen-r <janssen.r@stu.janesville.k12.wi.us>
2025-02-26 09:32:49 -0600

    Update 2/26

--- cutscene_1.gd
+++ cutscene_1.gd
@@ -1,7 +1,16 @@
 extends Node3D
 
+@onready var alarm = $AudioStreamPlayer3D
+
 func _ready() -> void:
+	
+	await alarm.finished
+	alarm.position = [9.189, 1.413, 9.205]
+	alarm.position_x += randf_range(-20,20)
+	alarm.position_y += randf_range(-20,20)
+	alarm.position_z += randf_range(-20,20)
+	
 	await get_tree().create_timer($AnimationPlayer.current_animation_length).timeout
 	get_tree().quit()
-	pass
+	
 	
